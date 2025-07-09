fb9d9a9279241dcf15dbade244a0e68c31a32030
Fulvia Mourikis <zum281@gmail.com>
2025-07-09 20:36:09 +0200

    tests: more tests for getFrameworkFromId

--- src/lib/__tests__/getFrameworkFromId.utils.test.ts
+++ src/lib/__tests__/getFrameworkFromId.utils.test.ts
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest";
 import { getFrameworkFromId } from "../utils";
 import React from "@/assets/react.svg";
 import Vue from "@/assets/vue.svg";
+import type { FeFrameworkKey } from "@/types";
 
 describe("getFrameworkFromId", () => {
   it("should return the correct framework object for a valid id 'react'", () => {
@@ -21,4 +22,26 @@ describe("getFrameworkFromId", () => {
       logo: Vue,
     });
   });
+});
+
+it("should return correct framework for all valid IDs", () => {
+  expect(getFrameworkFromId("alpine")).toBeDefined();
+  expect(getFrameworkFromId("angular")).toBeDefined();
+  expect(getFrameworkFromId("astro")).toBeDefined();
+  expect(getFrameworkFromId("fresh")).toBeDefined();
+  expect(getFrameworkFromId("gatsby")).toBeDefined();
+  expect(getFrameworkFromId("lit")).toBeDefined();
+  expect(getFrameworkFromId("nextjs")).toBeDefined();
+  expect(getFrameworkFromId("nuxtjs")).toBeDefined();
+  expect(getFrameworkFromId("qwik")).toBeDefined();
+  expect(getFrameworkFromId("react")).toBeDefined();
+  expect(getFrameworkFromId("remix")).toBeDefined();
+  expect(getFrameworkFromId("solidjs")).toBeDefined();
+  expect(getFrameworkFromId("svelte")).toBeDefined();
+  expect(getFrameworkFromId("sveltekit")).toBeDefined();
+  expect(getFrameworkFromId("vue")).toBeDefined();
+});
+
+it("should return undefined for invalid framework ID", () => {
+  expect(getFrameworkFromId("invalid" as FeFrameworkKey)).toBeUndefined();
 });
