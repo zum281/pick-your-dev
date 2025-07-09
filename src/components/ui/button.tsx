29d3413dd366e6a00b357e477bb14543644bcfb1
Fulvia Mourikis <zum281@gmail.com>
2025-07-09 14:36:50 +0200

    feat: add ui components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,29 +1,58 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import type { VariantProps } from "class-variance-authority";
-
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "./button.variants";
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+import React from "react";
+import type { ButtonHTMLAttributes } from "react";
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean;
-  }) {
-  const Comp = asChild ? Slot : "button";
+const buttonVariants = cva(
+  "font-head transition-all outline-hidden cursor-pointer duration-200 font-medium flex items-center",
+  {
+    variants: {
+      variant: {
+        default:
+          "shadow-md hover:shadow-none bg-primary text-black border-2 border-black transition hover:translate-y-1 hover:bg-primary-hover",
+        secondary:
+          "shadow-md hover:shadow-none bg-secondary shadow-primary text-secondary-foreground border-2 border-black transition hover:translate-y-1",
+        outline:
+          "shadow-md hover:shadow-none bg-transparent border-2 transition hover:translate-y-1",
+        link: "bg-transparent hover:underline",
+      },
+      size: {
+        sm: "px-3 py-1 text-sm shadow hover:shadow-none",
+        md: "px-4 py-1.5 text-base",
+        lg: "px-8 py-3 text-lg",
+        icon: "p-2",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+      variant: "default",
+    },
+  },
+);
 
-  return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
-  );
-}
+export interface IButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
-export { Button };
+export const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
+  (
+    {
+      children,
+      size = "md",
+      className = "",
+      variant = "default",
+      ...props
+    }: IButtonProps,
+    forwardedRef,
+  ) => (
+    <button
+      ref={forwardedRef}
+      className={cn(buttonVariants({ variant, size }), className)}
+      {...props}>
+      {children}
+    </button>
+  ),
+);
+
+Button.displayName = "Button";
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,92 +1,49 @@
-import * as React from "react"
+import { cn } from "@/lib/utils";
+import type { HTMLAttributes } from "react";
+import { Text } from "./text";
 
-import { cn } from "@/lib/utils"
+interface ICardProps extends HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
 
-function Card({ className, ...props }: React.ComponentProps<"div">) {
+const Card = ({ className, ...props }: ICardProps) => {
   return (
     <div
-      data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
-        className
+        "inline-block border-2 shadow-md transition-all hover:shadow-xs bg-card",
+        className,
       )}
       {...props}
     />
-  )
-}
+  );
+};
 
-function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
+const CardHeader = ({ className, ...props }: ICardProps) => {
   return (
     <div
-      data-slot="card-header"
-      className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
-        className
-      )}
+      className={cn("flex flex-col justify-start p-4", className)}
       {...props}
     />
-  )
-}
+  );
+};
 
-function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
-      {...props}
-    />
-  )
-}
+const CardTitle = ({ className, ...props }: ICardProps) => {
+  return <Text as="h3" className={cn("mb-2", className)} {...props} />;
+};
 
-function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-description"
-      className={cn("text-muted-foreground text-sm", className)}
-      {...props}
-    />
-  )
-}
+const CardDescription = ({ className, ...props }: ICardProps) => (
+  <p className={cn("text-muted-foreground", className)} {...props} />
+);
 
-function CardAction({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-action"
-      className={cn(
-        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+const CardContent = ({ className, ...props }: ICardProps) => {
+  return <div className={cn("p-4", className)} {...props} />;
+};
 
-function CardContent({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-content"
-      className={cn("px-6", className)}
-      {...props}
-    />
-  )
-}
+const CardComponent = Object.assign(Card, {
+  Header: CardHeader,
+  Title: CardTitle,
+  Description: CardDescription,
+  Content: CardContent,
+});
 
-function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
-      {...props}
-    />
-  )
-}
-
-export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardAction,
-  CardDescription,
-  CardContent,
-}
+export { CardComponent as Card };
