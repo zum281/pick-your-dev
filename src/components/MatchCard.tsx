0863b63fbcd0e6f438b857bcb5693ea1e9f7e3b6
Fulvia Mourikis <zum281@gmail.com>
2025-07-09 14:37:10 +0200

    feat: create MatchCard component

---
+++ src/components/MatchCard.tsx
@@ -0,0 +1,30 @@
+import type { FeFrameworkKey } from "@/types";
+import type { FC } from "react";
+import { Card } from "@/components/ui/card";
+import { Text } from "@/components/ui/text";
+import { getFrameworkFromId } from "@/lib/utils";
+
+export const MatchCard: FC<MatchCardProps> = ({ frameworkId, onSelect }) => {
+  const framework = getFrameworkFromId(frameworkId);
+  return (
+    <Card
+      role="button"
+      onClick={onSelect}
+      className="space-y-2 p-4 cursor-pointer">
+      <img
+        aria-labelledby={framework.id}
+        src={framework.logo}
+        height="128"
+        className="aspect-square h-32"
+      />
+      <Text id={framework.id} className="text-center font-medium">
+        {framework.name}
+      </Text>
+    </Card>
+  );
+};
+
+type MatchCardProps = {
+  frameworkId: FeFrameworkKey;
+  onSelect: () => void;
+};
